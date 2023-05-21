e827d78d5470328edfc8de23beafc25ed8c320b0
M Zubair <md.zubair143598@gmail.com>
2023-05-21 10:53:44 +0500

    init4

--- Components/Tabs.jsx
+++ Components/Tabs.jsx
@@ -8,7 +8,7 @@ const Tabs = () => {
     const [tab, setTab] =useState(0)
     if(tab===0) return <Auth setTab={setTab}/>
     if(tab===1) return <SignIn setTab={setTab}/>
-
+//asdasd
   return 
     null
   
